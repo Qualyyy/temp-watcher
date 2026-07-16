2bd88de765297a5d4c3b977ebebdadd81b1bf4c6
Qualyyy <Jarne.QUALY@student.howest.be>
2026-07-16 22:09:07 +0200

    Print all temperature sensors

--- Temp Watcher/Program.cs
+++ Temp Watcher/Program.cs
@@ -1,1 +1,26 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using LibreHardwareMonitor.Hardware;
+
+Computer computer = new Computer
+{
+    IsCpuEnabled = true,
+    IsGpuEnabled = true
+};
+
+computer.Open();
+
+foreach (IHardware hardware in computer.Hardware)
+{
+    Console.WriteLine($"Hardware: {hardware.Name}");
+
+    hardware.Update();
+
+    foreach (ISensor sensor in hardware.Sensors)
+    {
+        if (sensor.SensorType == SensorType.Temperature)
+        {
+            Console.WriteLine($"{sensor.Name}: {sensor.Value}°C");
+        }
+    }
+
+    Console.WriteLine();
+}
