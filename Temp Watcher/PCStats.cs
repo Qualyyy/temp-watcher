b8e6505e589dd18e3d0cd9dfca45e1b40cdcf152
Qualyyy <Jarne.QUALY@student.howest.be>
2026-07-16 22:33:41 +0200

    Only get CPU and GPU temps

---
+++ Temp Watcher/PCStats.cs
@@ -0,0 +1,8 @@
+﻿namespace Temp_Watcher
+{
+    internal class PCStats
+    {
+        public float CPUTemperature { get; set; }
+        public float GPUTemperature { get; set; }
+    }
+}
--- Temp Watcher/Program.cs
+++ Temp Watcher/Program.cs
@@ -1,4 +1,5 @@
 ﻿using LibreHardwareMonitor.Hardware;
+using Temp_Watcher;
 
 Computer computer = new Computer
 {
@@ -8,19 +9,56 @@ Computer computer = new Computer
 
 computer.Open();
 
+PCStats stats = new PCStats();
+
 foreach (IHardware hardware in computer.Hardware)
 {
-    Console.WriteLine($"Hardware: {hardware.Name}");
-
     hardware.Update();
 
-    foreach (ISensor sensor in hardware.Sensors)
+    switch (hardware.HardwareType)
     {
-        if (sensor.SensorType == SensorType.Temperature)
-        {
-            Console.WriteLine($"{sensor.Name}: {sensor.Value}°C");
-        }
+        case HardwareType.Cpu:
+            float CPUTemp = GetCPUTemp(hardware);
+            Console.WriteLine($"CPU: {CPUTemp}°C");
+            stats.CPUTemperature = CPUTemp;
+            break;
+
+        case HardwareType.GpuNvidia:
+            float GPUTemp = GetGPUTemp(hardware);
+            Console.WriteLine($"GPU: {GPUTemp}°C");
+            stats.GPUTemperature = GPUTemp;
+            break;
     }
 
     Console.WriteLine();
+}
+
+float GetCPUTemp(IHardware hardware)
+{
+    foreach (ISensor sensor in hardware.Sensors)
+    {
+        if (sensor.Value != null
+            && sensor.SensorType == SensorType.Temperature
+            && sensor.Name.Contains("Average"))
+        {
+            return (float)sensor.Value;
+        }
+    }
+
+    throw new Exception("CPU temp not found");
+}
+
+float GetGPUTemp(IHardware hardware)
+{
+    foreach (ISensor sensor in hardware.Sensors)
+    {
+        if (sensor.Value != null
+            && sensor.SensorType == SensorType.Temperature
+            && sensor.Name.Contains("Core"))
+        {
+            return (float)sensor.Value;
+        }
+    }
+
+    throw new Exception("GPU temp not found");
 }
