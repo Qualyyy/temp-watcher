37199ab63bd645394e4cd298991987ae2e80ee1c
Qualyyy <Jarne.QUALY@student.howest.be>
2026-07-16 23:23:47 +0200

    Use new class

--- Temp Watcher/Program.cs
+++ Temp Watcher/Program.cs
@@ -11,56 +11,12 @@ computer.Open();
 
 while (true)
 {
-    PCStats stats = new PCStats();
 
-    foreach (IHardware hardware in computer.Hardware)
-    {
-        hardware.Update();
+    HardwareMonitor monitor = new HardwareMonitor();
 
-        switch (hardware.HardwareType)
-        {
-            case HardwareType.Cpu:
-                stats.CPUTemperature = GetCPUTemp(hardware);
-                break;
-
-            case HardwareType.GpuNvidia:
-                stats.GPUTemperature = GetGPUTemp(hardware);
-                break;
-        }
-    }
+    PCStats stats = monitor.getStats();
 
     Console.WriteLine(stats.ToJson());
 
     Thread.Sleep(1000);
-}
-
-float GetCPUTemp(IHardware hardware)
-{
-    foreach (ISensor sensor in hardware.Sensors)
-    {
-        if (sensor.Value != null
-            && sensor.SensorType == SensorType.Temperature
-            && sensor.Name.Contains("Max"))
-        {
-            sensor.Hardware.Update();
-            return (float)sensor.Value;
-        }
-    }
-
-    return -1;
-}
-
-float GetGPUTemp(IHardware hardware)
-{
-    foreach (ISensor sensor in hardware.Sensors)
-    {
-        if (sensor.Value != null
-            && sensor.SensorType == SensorType.Temperature
-            && sensor.Name.Contains("Core"))
-        {
-            return (float)sensor.Value;
-        }
-    }
-
-    return -1;
 }
