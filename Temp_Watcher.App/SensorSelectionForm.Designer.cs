b68006d565e1b05210165f8ccf7f05df3defd9bf
Qualyyy <qualyj@gmail.com>
2026-09-03 19:17:29 +0200

    Add form to select sensors

---
+++ Temp_Watcher.App/SensorSelectionForm.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Temp_Watcher.App
+{
+    partial class SensorSelectionForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblCpu = new Label();
+            cmbCpuSensor = new ComboBox();
+            cmbGpuSensor = new ComboBox();
+            lblGpu = new Label();
+            btnSave = new Button();
+            btnCancel = new Button();
+            SuspendLayout();
+            // 
+            // lblCpu
+            // 
+            lblCpu.AutoSize = true;
+            lblCpu.Location = new Point(221, 22);
+            lblCpu.Name = "lblCpu";
+            lblCpu.Size = new Size(82, 20);
+            lblCpu.TabIndex = 0;
+            lblCpu.Text = "CPU sensor";
+            // 
+            // cmbCpuSensor
+            // 
+            cmbCpuSensor.FormattingEnabled = true;
+            cmbCpuSensor.Location = new Point(221, 89);
+            cmbCpuSensor.Name = "cmbCpuSensor";
+            cmbCpuSensor.Size = new Size(151, 28);
+            cmbCpuSensor.TabIndex = 1;
+            // 
+            // cmbGpuSensor
+            // 
+            cmbGpuSensor.FormattingEnabled = true;
+            cmbGpuSensor.Location = new Point(221, 231);
+            cmbGpuSensor.Name = "cmbGpuSensor";
+            cmbGpuSensor.Size = new Size(151, 28);
+            cmbGpuSensor.TabIndex = 3;
+            // 
+            // lblGpu
+            // 
+            lblGpu.AutoSize = true;
+            lblGpu.Location = new Point(221, 175);
+            lblGpu.Name = "lblGpu";
+            lblGpu.Size = new Size(83, 20);
+            lblGpu.TabIndex = 2;
+            lblGpu.Text = "GPU sensor";
+            // 
+            // btnSave
+            // 
+            btnSave.Location = new Point(508, 344);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(94, 29);
+            btnSave.TabIndex = 4;
+            btnSave.Text = "Save";
+            btnSave.UseVisualStyleBackColor = true;
+            btnSave.Click += btnSave_Click;
+            // 
+            // btnCancel
+            // 
+            btnCancel.Location = new Point(299, 344);
+            btnCancel.Name = "btnCancel";
+            btnCancel.Size = new Size(94, 29);
+            btnCancel.TabIndex = 5;
+            btnCancel.Text = "Cancel";
+            btnCancel.UseVisualStyleBackColor = true;
+            btnCancel.Click += btnCancel_Click;
+            // 
+            // SensorSelectionForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnCancel);
+            Controls.Add(btnSave);
+            Controls.Add(cmbGpuSensor);
+            Controls.Add(lblGpu);
+            Controls.Add(cmbCpuSensor);
+            Controls.Add(lblCpu);
+            Name = "SensorSelectionForm";
+            Text = "SensorSelectionForm";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblCpu;
+        private ComboBox cmbCpuSensor;
+        private ComboBox cmbGpuSensor;
+        private Label lblGpu;
+        private Button btnSave;
+        private Button btnCancel;
+    }
+}
---
+++ Temp_Watcher.App/SensorSelectionForm.cs
@@ -0,0 +1,58 @@
+﻿using LibreHardwareMonitor.Hardware;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Temp_Watcher.Core;
+
+namespace Temp_Watcher.App
+{
+    public partial class SensorSelectionForm : Form
+    {
+
+        public ISensor? SelectedCpuSensor { get; private set; }
+        public ISensor? SelectedGpuSensor { get; private set; }
+
+
+        public SensorSelectionForm(HardwareMonitor monitor)
+        {
+            InitializeComponent();
+
+            cmbCpuSensor.DataSource = monitor.CpuTemperatureSensors;
+            cmbCpuSensor.DisplayMember = nameof(ISensor.Name);
+
+            if (monitor.CpuTemperatureSensors.Count == 0)
+            {
+                cmbCpuSensor.Enabled = false;
+                lblCpu.Text = "CPU sensor (unavailable)";
+            }
+
+            cmbGpuSensor.DataSource = monitor.GpuTemperatureSensors;
+            cmbGpuSensor.DisplayMember = nameof(ISensor.Name);
+
+            if (monitor.GpuTemperatureSensors.Count == 0)
+            {
+                cmbGpuSensor.Enabled = false;
+                lblGpu.Text = "GPU sensor (unavailable)";
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SelectedCpuSensor = cmbCpuSensor.SelectedItem as ISensor;
+            SelectedGpuSensor = cmbGpuSensor.SelectedItem as ISensor;
+
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
+        }
+    }
+}
